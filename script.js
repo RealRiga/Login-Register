13e83963593b8f9b480b33b2001c22d4e20a4b7f
RealRiga <116930426+RealRiga@users.noreply.github.com>
2023-07-20 23:42:58 -0500

    working

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
+
 
 registerLink.addEventListener('click', ()=> {
     wrapper.classList.add('active');
@@ -10,6 +13,10 @@ loginLink.addEventListener('click', ()=>{
     wrapper.classList.remove('active');
 });
 
-// form-box-register.addEventListener('click', ()=>{
-//         // wrapper.classList.
-//     });
+btnPopup.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup');
+});
